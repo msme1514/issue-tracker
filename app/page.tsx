d99672976e27c25dd9aff90e57153eb697be17e8
Mustafa Mohammed <mmus1514@gmail.com>
2024-08-21 11:47:41 -0700

    add prisma adapter

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import prisma from "@/prisma/client";
-import LatestIssues from "./LatestIssues";
+import IssueChart from "./IssueChart";
 import IssueSummary from "./IssueSummary";
+import LatestIssues from "./LatestIssues";
+import { Grid, Flex } from "@radix-ui/themes";
 
 export default async function Home() {
   const open = await prisma.issue.count({
@@ -14,8 +16,13 @@ export default async function Home() {
   });
   return (
     <>
-      <LatestIssues />
-      <IssueSummary open={open} inProgress={inProgress} closed={closed} />
+      <Grid columns={{ initial: "1", md: "2" }} gap="5">
+        <Flex direction="column" gap="5">
+          <IssueSummary open={open} inProgress={inProgress} closed={closed} />
+          <IssueChart open={open} inProgress={inProgress} closed={closed} />
+        </Flex>
+        <LatestIssues />
+      </Grid>
     </>
   );
 }
