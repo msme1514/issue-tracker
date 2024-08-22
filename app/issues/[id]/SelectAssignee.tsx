9b6e5a427dcbe8919af890e5dca04e9f548625f4
Mustafa Mohammed <mmus1514@gmail.com>
2024-08-22 05:16:09 -0700

    fetch users data with react query

--- app/issues/[id]/SelectAssignee.tsx
+++ app/issues/[id]/SelectAssignee.tsx
@@ -5,23 +5,38 @@ import { useSession } from "next-auth/react";
 import React, { useEffect, useState } from "react";
 import { User } from "@prisma/client";
 import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
 
 const SelectAssignee = () => {
-  const [users, setUsers] = useState<User[]>([]);
-  useEffect(() => {
-    const fetchUsers = async () => {
-      const { data } = await axios.get<User[]>("/api/users");
-      setUsers(data);
-    };
-    fetchUsers();
-  }, []);
+  const {
+    data: users,
+    isLoading,
+    error,
+  } = useQuery<User[]>({
+    queryKey: ["users"],
+    queryFn: () => axios.get("/api/users").then((res) => res.data),
+    staleTime: 60 * 60 * 1000, //60 mins
+    retry: 3,
+  });
+  //   const [users, setUsers] = useState<User[]>([]);
+  //   useEffect(() => {
+  //     const fetchUsers = async () => {
+  //       const { data } = await axios.get<User[]>("/api/users");
+  //       setUsers(data);
+  //     };
+  //     fetchUsers();
+  //   }, []);
+
+  if (isLoading) return null;
+
+  if (error) return null;
   return (
     <Select.Root>
       <Select.Trigger placeholder="Assign..." />
       <Select.Content>
         <Select.Group>
           <Select.Label>Assignee</Select.Label>
-          {users.map((user) => (
+          {users?.map((user) => (
             <Select.Item key={user.id} value={user.id}>
               {user.name}
             </Select.Item>
